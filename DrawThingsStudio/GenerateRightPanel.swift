99e81da023b66450550eab7b194b3d96227cfcff
Ned Baugh <ned.baugh@gmail.com>
2026-04-12 14:48:33 -0400

    fix: crop-to-zoom X offset sign

--- DrawThingsStudio/GenerateRightPanel.swift
+++ DrawThingsStudio/GenerateRightPanel.swift
@@ -800,7 +800,7 @@ fileprivate func croppedCanvasImage(image: NSImage?,
     let visibleH = canvasH / canvasScale
     let centerX = canvasW / 2
     let centerY = canvasH / 2
-    let visibleOriginX = centerX - visibleW / 2 + canvasOffset.width  / canvasScale
+    let visibleOriginX = centerX - visibleW / 2 - canvasOffset.width  / canvasScale
     let visibleOriginY = centerY - visibleH / 2 + canvasOffset.height / canvasScale
 
     let clipX    = max(visibleOriginX, fittedOriginX)
